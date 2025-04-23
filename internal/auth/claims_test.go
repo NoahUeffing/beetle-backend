7f9ec05edee1072442d10f7c17c94ea70585a29d
Noah Ueffing <noahueffing@gmail.com>
2025-04-23 11:34:57 -0300

    test claims

---
+++ internal/auth/claims_test.go
@@ -0,0 +1,76 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCombinedClaims_Valid(t *testing.T) {
+	now := time.Now().UTC()
+	userId := uuid.New()
+
+	tests := []struct {
+		name    string
+		claims  *CombinedClaims
+		wantErr error
+	}{
+		{
+			name: "valid token",
+			claims: &CombinedClaims{
+				ClaimsData: ClaimsData{
+					UserId: userId,
+				},
+				RegisteredClaims: jwt.RegisteredClaims{
+					ExpiresAt: jwt.NewNumericDate(now.Add(time.Hour)),
+					NotBefore: jwt.NewNumericDate(now.Add(-time.Hour)),
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "expired token",
+			claims: &CombinedClaims{
+				ClaimsData: ClaimsData{
+					UserId: userId,
+				},
+				RegisteredClaims: jwt.RegisteredClaims{
+					ExpiresAt: jwt.NewNumericDate(now.Add(-time.Hour)),
+				},
+			},
+			wantErr: jwt.ErrTokenExpired,
+		},
+		{
+			name: "token not yet valid",
+			claims: &CombinedClaims{
+				ClaimsData: ClaimsData{
+					UserId: userId,
+				},
+				RegisteredClaims: jwt.RegisteredClaims{
+					NotBefore: jwt.NewNumericDate(now.Add(time.Hour)),
+				},
+			},
+			wantErr: jwt.ErrTokenNotValidYet,
+		},
+		{
+			name: "token with no time constraints",
+			claims: &CombinedClaims{
+				ClaimsData: ClaimsData{
+					UserId: userId,
+				},
+				RegisteredClaims: jwt.RegisteredClaims{},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.claims.Valid()
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
