f4dc677b68c85518d09be79e69e8e3ea8ad37c91
Noah Ueffing <noahueffing@gmail.com>
2025-04-28 11:18:19 -0300

    add update user

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -91,3 +91,35 @@ func GetUser(c Context) error {
 
 	return c.JSON(http.StatusOK, user)
 }
+
+// UpdateUser godoc
+// @Summary Update a user
+// @Description Update a user's details
+// @Tags user
+// @Accept json
+// @Produce json
+// @Param id path string true "User ID"
+// @Param user body domain.User true "User object"
+// @Success 200 {object} domain.User
+// @Failure 400 {string} string "Bad request"
+// @Failure 401 {string} string "Unauthorized"
+// @Failure 404 {string} string "User not found"
+// @Failure 500 {string} string "Internal server error"
+// @Router /user/{id} [put]
+// @Security JWTToken
+func UpdateUser(c Context) error {
+	u := &domain.User{}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+
+	if formErrs := c.validate(u); formErrs != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, formErrs)
+	}
+
+	u, err := c.UserService.Update(u)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, u)
+}
